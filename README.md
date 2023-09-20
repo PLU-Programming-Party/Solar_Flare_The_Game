530fb218d2f2c9498a3a72eb1b8dfb0482759124
ryantmitchell <116308490+ryantmitchell@users.noreply.github.com>
2023-09-19 22:06:07 -0700

    A tiny drop of progress was made

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Run These Commands:
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+# Download the dataset from:
+# https://www.kaggle.com/datasets/arashnic/soalr-wind?resource=download
+
+
+    
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pandas as pd
 import matplotlib as mp
+import zipfile
+
+ARCHIVE_PATH = "data/archive.zip"
+DESTINATION_PATH = "data/"
+def un_zipper(zip_path: str, dest_path: str):
+
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        # Extract all the contents of the zip file to the specified directory
+        zip_ref.extractall(dest_path)
+
+un_zipper(ARCHIVE_PATH, DESTINATION_PATH)
 
