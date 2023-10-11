ac08a6081d3fbed2a0617e2a8908ead52a71e413
Noatak Post <noatak.post@gmail.com>
2023-10-10 22:08:58 -0700

    got confused about gse

--- main.py
+++ main.py
@@ -39,28 +39,48 @@ def two_dimention_timeplot():
 
     #period_a["timedelta"][:100].map(str_to_timedelta), period_a["dst"][:100],
     plt.plot(period_a["timedelta"][:100].map(str_to_timedelta), period_a["dst"][:100], solar_wind_pd_a["timedelta"][:6000].map(str_to_timedelta), solar_wind_pd_a["density"][:6000])
+def function_for_plotting_the_satellite_position():
+    #Create 3D plot of satellite positional data
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    satellite_pos = pd.read_csv("data/satellite_pos.csv")
+    period_a_sp = satellite_pos[satellite_pos['period'] == 'train_a']
+    period_b_sp = satellite_pos[satellite_pos['period'] == 'train_b']
+    period_c_sp = satellite_pos[satellite_pos['period'] == 'train_c']
+    size = -1
+    gse_x = period_b_sp["gse_x_ace"][0:size]
+    gse_y = period_b_sp["gse_y_ace"][0:size]
+    gse_z = period_b_sp["gse_z_ace"][0:size]
+    ax.scatter(gse_x, gse_y, gse_z)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+    #min = pd.concat([gse_x, gse_y, gse_z]).min()
+    #max = pd.concat([gse_x, gse_y, gse_z]).max()
+    #ax.set_xlim3d(min, max)
+    #ax.set_ylim3d(min, max)
+    #ax.set_zlim3d(min, max)
+    #ax.set_aspect('equal', adjustable='box')
+    #ax.view_init(elev=30, azim=45)
+    #gse_x_median = gse_x.median()
+    #gse_y_median = gse_y.median()
+    #gse_z_median = gse_z.median()
+    plt.show()
 
-#Create 3D plot of satellite positional data
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-satellite_pos = pd.read_csv("data/satellite_pos.csv")
-period_a_sp = satellite_pos[satellite_pos['period'] == 'train_a']
-period_b_sp = satellite_pos[satellite_pos['period'] == 'train_b']
-period_c_sp = satellite_pos[satellite_pos['period'] == 'train_c']
-size = -1
-gse_x = period_b_sp["gse_x_ace"][0:size]
-gse_y = period_b_sp["gse_y_ace"][0:size]
-gse_z = period_b_sp["gse_z_ace"][0:size]
+rowskip = 3600
+solar_data = pd.read_csv("data/solar_wind.csv")
+solar_data = solar_data.iloc[rowskip:]
+period_a_sp = solar_data[solar_data['period'] == 'train_a']
+period_b_sp = solar_data[solar_data['period'] == 'train_b']
+period_c_sp = solar_data[solar_data['period'] == 'train_c']
+size = 1000
+gse_x = [x for n, x in enumerate(period_b_sp["bx_gse"]) if n % rowskip == 0]
+gse_y = [x for n, x in enumerate(period_b_sp["by_gse"]) if n % rowskip == 0]
+gse_z = [x for n, x in enumerate(period_b_sp["bz_gse"]) if n % rowskip == 0]
 ax.scatter(gse_x, gse_y, gse_z)
-#min = pd.concat([gse_x, gse_y, gse_z]).min()
-#max = pd.concat([gse_x, gse_y, gse_z]).max()
-#ax.set_xlim3d(min, max)
-#ax.set_ylim3d(min, max)
-#ax.set_zlim3d(min, max)
-#ax.set_aspect('equal', adjustable='box')
-#ax.view_init(elev=30, azim=45)
-#gse_x_median = gse_x.median()
-#gse_y_median = gse_y.median()
-#gse_z_median = gse_z.median()
-
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
 plt.show()
